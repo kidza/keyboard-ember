b73067b247ba5da8faeb6d2e32bbea1c34c8c287
dzaki <dzaivan@gmail.com>
2017-03-07 14:17:41 +0100

    #Adding mouse down, mouse up events on keyboard

--- app/components/tone-element.js
+++ app/components/tone-element.js
@@ -2,5 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     classNameBindings: ['isWholeTone:whole-tone:half-tone'],
-    isWholeTone: true
+    isWholeTone: true,
+
+    mouseDown(event) {
+    	let element = event.target;
+        if (element.className.match(/tone-text-element/)) {
+            element = element.parentElement;
+        }
+        element.classList.add('pressedTone');
+       let matches = element.querySelectorAll("span");
+        if (matches.length > 0) {
+            let tone = matches[0].innerHTML;
+            //playAudioTone(tone);
+            let playedTones = document.getElementById('tone-played').value;
+            if (playedTones == "") {
+                playedTones = tone;
+            } else {
+                playedTones = playedTones + " " + tone;
+            }
+            document.getElementById('tone-played').value = playedTones;
+        }
+    },
+
+    mouseUp(event) {
+    	let element = event.target;
+        if (element.className.match(/tone-text-element/)) {
+            element = element.parentElement;
+        }
+        element.classList.remove('pressedTone');
+    }
 });
